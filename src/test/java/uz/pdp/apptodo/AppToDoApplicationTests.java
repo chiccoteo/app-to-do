86d2fbbd9b62c74530afb9f183595def3751b748
Ramziddin <xolmaxmatovramziddin7@gmail.com>
2022-06-15 15:22:43 +0500

    first

---
+++ src/test/java/uz/pdp/apptodo/AppToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.apptodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppToDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
